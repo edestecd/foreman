f0a49678f7715251c3488dcc096c2100c89cba6b
Chris Edester <edestecd@miamioh.edu>
2014-02-15 22:09:59 -0500

    Add staging environment

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,7 +9,7 @@ unless Foreman::Application.config.secret_token
   token = File.read(token_store) if File.exist? token_store
   unless token
     token = secure_token
-    File.open(token_store, "w", 0600) { |f| f.write(token) }
+    File.open(token_store, "w", 0640) { |f| f.write(token) }
   end
   Foreman::Application.config.secret_token = token
 end
