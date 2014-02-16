751f1316716abaebf8924bb43e77d7370a8f3358
Chris Edester <edestecd@miamioh.edu>
2014-02-16 13:41:41 -0500

    mysql cluster does not support this

--- db/migrate/20130520161514_add_unique_constraint_to_fact_name.rb
+++ db/migrate/20130520161514_add_unique_constraint_to_fact_name.rb
@@ -1,7 +1,7 @@
 class AddUniqueConstraintToFactName < ActiveRecord::Migration
   def self.up
     remove_index(:fact_names, :column => :name) rescue nil
-    options = ActiveRecord::Base.connection.instance_values["config"][:adapter].grep(/mysql/).any? ?
+    options = ActiveRecord::Base.connection.instance_values["config"][:adapter].grep(/not/).any? ?
       { :unique => true, :length => 254 } :
       { :unique => true }
     add_index(:fact_names, :name, options)
--- db/migrate/20131021152315_change_name_index_on_fact_name.rb
+++ db/migrate/20131021152315_change_name_index_on_fact_name.rb
@@ -1,7 +1,7 @@
 class ChangeNameIndexOnFactName < ActiveRecord::Migration
   def self.up
     remove_index :fact_names, :column => :name, :unique => true
-    options = ActiveRecord::Base.connection.instance_values["config"][:adapter].grep(/mysql/).any? ?
+    options = ActiveRecord::Base.connection.instance_values["config"][:adapter].grep(/not/).any? ?
         { :unique => true, :length => 254 } :
         { :unique => true }
     add_index :fact_names, [:name, :type], options
@@ -9,7 +9,7 @@ class ChangeNameIndexOnFactName < ActiveRecord::Migration
 
   def self.down
     remove_index :fact_names, [:name, :type]
-    options = ActiveRecord::Base.connection.instance_values["config"][:adapter].grep(/mysql/).any? ?
+    options = ActiveRecord::Base.connection.instance_values["config"][:adapter].grep(/not/).any? ?
         { :unique => true, :length => 254 } :
         { :unique => true }
     add_index :fact_names, :name, options
